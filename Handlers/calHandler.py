580ccd0713762c9f97c23ad932e249a4998a8c1f
piroj <p1r0j@pm.me>
2024-09-03 20:28:56 -0400

    Finished set date functionality.

--- Handlers/calHandler.py
+++ Handlers/calHandler.py
@@ -24,18 +24,74 @@ if os.path.exists(CALSAVE):
         dateDynamic = calLoad
 
 
+# Check date formatting.
+def check_date_formatting(actual, expected):
+    return (actual.isdigit() and len(actual) == expected)
+
+
+# Set manual date.
+def set_date_manual():
+    global dateDynamic
+    console.print(FmStr.fEMPTY)
+    year = Prompt.ask(f"{FmStr.fPROMPT} Enter the year (YYYY)")
+    if not check_date_formatting(year, 4):
+        console.print(FmStr.fEMPTY)
+        console.print(f"{FmStr.fERROR} Invalid year.")
+    else:
+        console.print(FmStr.fEMPTY)
+        month = Prompt.ask(f"{FmStr.fPROMPT} Enter the month (MM)")
+        if not check_date_formatting(month, 2):
+            console.print(FmStr.fEMPTY)
+            console.print(f"{FmStr.fERROR} Invalid month.")
+        else:
+            console.print(FmStr.fEMPTY)
+            day = Prompt.ask(f"{FmStr.fPROMPT} Enter the day (DD)")
+            if not check_date_formatting(day, 2):
+                console.print(FmStr.fEMPTY)
+                console.print(f"{FmStr.fERROR} Invalid day.")
+            else:
+                console.print(f"{FmStr.fOK}  Recording new date...")
+                dateDynamic = {
+                        'Year': year,
+                        'Month': month,
+                        'Day': day
+                        }
+                save_cal()
+
+
+# Set auto date.
+def set_date_auto():
+    global dateDynamic
+    console.print(f"{FmStr.fOK}  Switching to {FmStr.wAUTO} mode...")
+    dateDynamic = "Auto"
+    save_cal()
+
+
 # Confirm set date.
 def confirm_set_date(which):
     if which == "Manual":
         console.print(FmStr.fEMPTY)
-        console.print(f"{FmStr.fNEUTRAL} {FmStr.wTELLER} is currently set to {FmStr.wAUTO} mode.")
+        console.print(f"{FmStr.fNEUTRAL} The current date is set to {FmStr.wAUTO} mode.")
         console.print(FmStr.fEMPTY)
-        answer = Prompt.ask(f"{FmStr.fPROMPT} Switch to {FmStr.wMANUAL}? (Y/n) ")
+        answer = Prompt.ask(f"{FmStr.fPROMPT} Switch to {FmStr.wMANUAL} (Y/n)?")
+        if answer.lower() == "n":
+            exit()
+        else:
+            set_date_manual()
     else:
         console.print(FmStr.fEMPTY)
-        console.print(f"{FmStr.fNEUTRAL} {FmStr.wTELLER} is currently set to {FmStr.wMANUAL} mode.")
+        console.print(f"{FmStr.fNEUTRAL} The current date is set to {dateDynamic['Year']}-{dateDynamic['Month']}-{dateDynamic['Day']}.")
         console.print(FmStr.fEMPTY)
-        answer = Prompt.ask(f"{FmStr.fPROMPT} Switch to {FmStr.wAUTO}? (Y/n) ")
+        answer = Prompt.ask(f"{FmStr.fPROMPT} Switch to {FmStr.wAUTO} (Y/n)?")
+        if answer.lower() == "n":
+            console.print(FmStr.fEMPTY)
+            answer = Prompt.ask(f"{FmStr.fPROMPT} Update {FmStr.wMANUAL} date (Y/n)?")
+            if answer.lower() == "n":
+                exit()
+            else:
+                set_date_manual()
+        else:
+            set_date_auto()
 
 
 # Process set date.
--- Resources/fmStrings.py
+++ Resources/fmStrings.py
@@ -7,6 +7,7 @@ fEMPTY = " |   |"
 fNEUTRAL = "[[bold cyan]~'_'~[/bold cyan]]"
 fPROMPT = "([bold magenta]~[ ]~[/bold magenta])"
 fERROR = "[[bold red]ERROR[/bold red]]"
+fOK = " |[green]OK![/green]|"
 # Formatted words.
 wTELLER = "[bold cyan]tEller[/bold cyan]"
 wAUTO = "[bold blue]Auto[/bold blue]"
